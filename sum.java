21732f0edd99e32e1a39ec074ab5f6c9a8835886
salmansheri <95226945+salmansheri@users.noreply.github.com>
2022-09-24 21:01:24 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,12 @@
+public class sum {
+    public static void main(String[] args) {
+
+        int sum = 0;
+        for (int i=0; i < 50+1; i++) {
+            sum = sum + i;
+
+        }
+        System.out.println("Sum of 50 numbers is " + sum);
+
+    }
+}
